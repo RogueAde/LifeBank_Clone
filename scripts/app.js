041dd22cbede70000695358c8d6c4be195b32619
rogueade <ademilddiq@gmail.com>
2021-09-11 14:31:18 +0100

    Did some major coding up in this commit, by the way SASS is wonderful

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,14 @@
 const menuIcon = document.querySelector(".hamburger img");
 const mainSectionNavs= document.querySelector(".main-section--article");
 const sideBar= document.querySelector(".main-sidebar");
+const allsections = document.querySelector("#allDisabled") ;
 
 
 function lilac() {
     if(menuIcon.getAttribute("src") == "images/hamburger-icon.svg"){
-        menuIcon.setAttribute("src", "images/close-hamburger-icon.svg")
+        document.querySelector(".hamburger").classList.add("fadeIn") ;
+        allsections.style.display = "none";
+        menuIcon.setAttribute("src", "images/close-hamburger-icon.svg") ;
         mainSectionNavs.style.display= "none" ;
         sideBar.style.display= "block";
         setTimeout(()=> {
@@ -14,6 +17,7 @@ function lilac() {
         
     } else{
         menuIcon.setAttribute("src", "images/hamburger-icon.svg")
+        allsections.style.display = "block";
         sideBar.style.display= "none";
         sideBar.style.opacity= "0";
         mainSectionNavs.style.display= "block" ;
