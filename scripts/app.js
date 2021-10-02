2c00ac6b00f09f7eec266a146ac9f2dcb056af5f
rogueade <ademilddiq@gmail.com>
2021-10-02 02:58:39 +0100

    Modified files and wrote new codes to enable scroll triggers

--- scripts/app.js
+++ scripts/app.js
@@ -23,4 +23,41 @@ function lilac() {
     }  
 }
 
+gsap.from(
+    "#section2 article",
+    {
+        scrollTrigger: {
+            trigger: "#section2",
+            start: "top 50%",
+            snap: 1/3
+           },
+        x: "-100vw",
+        duration: 2
+    }
+)
+gsap.from(
+    "#section3 article",
+    {
+        scrollTrigger: {
+            trigger: "#section3",
+            start: "top 50%",
+            snap: 1/3
+           },
+        y: "-100vh",
+        duration: 2
+    }
+)
+gsap.from(
+    "#section4 article",
+    {
+        scrollTrigger: {
+         trigger: "#section4",
+         start: "top 50%",
+         snap: 1/3
+        },
+        opacity: 0,
+        scale: 0.3,
+        duration: 2
+    }
+)
 
